cd3ee61e0f27676498c28d7cfd43eaf6208f0d22
NelloC <antonello.conelli@mail.polimi.it>
2024-10-29 22:53:03 +0000

    Caricamento dei file dal Google Drive

--- controller.py
+++ controller.py
@@ -2,20 +2,24 @@ import numpy as np
 from NeuronModel import NeuronModel
 
 class NeuronController:
-    def __init__(self, model, view):
-        self.NeuronModel = NeuronModel
+    def __init__(self, model: NeuronModel, view):
+        self.model = model  # Qui assegniamo l'istanza di NeuronModel
         self.view = view
 
     def visualize(self):
-        original_points = self.NeuronModel.get_points()
-        original_lines = self.NeuronModel.get_lines()
+        # Ottiene i punti e le linee originali
+        original_points = self.model.get_points()
+        original_lines = self.model.get_lines()
 
+        # Esegue il collegamento dei componenti
         self.model.connect_components()
 
-        connected_points = self.NeuronModel.get_connected_points()
-        connected_lines = self.NeuronModel.get_connected_lines()
-        stitch_lines = self.NeuronModel.get_stitch_lines()
+        # Ottiene i punti e le linee connessi
+        connected_points = self.model.get_connected_points()
+        connected_lines = self.model.get_connected_lines()
+        stitch_lines = self.model.get_stitch_lines()
 
+        # Visualizza i dati originali e connessi
         self.view.render_original_and_connected(
             original_points,
             original_lines,
@@ -25,12 +29,16 @@ class NeuronController:
         )
 
     def visualize_components(self):
+        # Visualizza i componenti connessi
         connected_components = self.model.connected_swc_samples
         self.view.render_connected_components(connected_components)
 
     def run(self):
+        # Avvia la visualizzazione
         self.view.show()
 
     def close(self):
+        # Chiude la visualizzazione
         self.view.close()
+
 
