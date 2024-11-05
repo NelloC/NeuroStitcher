4f54ef42234ff38747cf51feb802ddcbff51db6e
NelloC <antonello.conelli@mail.polimi.it>
2024-11-05 16:27:04 +0000

    Caricamento dei file dal Google Drive

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from storage import NeuronDataStorage
 from NeuronModel import NeuronModel
 from view import NeuronView
@@ -8,11 +9,17 @@ from vector_calculations import VectorCalculator
 from clustering import ClusteringManager
 from IPython.display import Image, display  # Per visualizzare l'immagine in Colab
 
+# Installa e avvia xvfb per il rendering offscreen
 print("Inizio esecuzione di main.py")
+print("Installazione e avvio di Xvfb...")
 
-# Avvio display virtuale (già incluso se stai usando Colab)
+!apt-get install -y xvfb > /dev/null 2>&1
+os.system("Xvfb :99 -screen 0 1024x768x24 &")
+os.environ["DISPLAY"] = ":99"
+
 print("Display virtuale attivato")
 
+# Caricamento dei dati
 print("Caricamento dati del neurone...")
 file_path = "data/ms1821_alignment_soloVPM_nocontours_neuritescorrected_aligned.json"
 unit_orientation_origin = ["um", "RAS", "corner"]
